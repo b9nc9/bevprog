22473bf85dd34f5e9b25d46157bea0c66f5d69c4
b9nc9 <b9nc9@protonmail.com>
2019-09-30 17:01:10 +0200

    Félreértelmezés
    A leírásban szereplő drill száma az a fejezetre utal, nem a drillen belül szereplő feladat számára.

--- forrasok/drill-ch4-3.cpp
+++ forrasok/drill-ch4-3.cpp
@@ -1,16 +1,70 @@
 #include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-        double a,b;
 
-        while(cin >> a >> b){
-                cout << "a: " << a << endl;
-                cout << "b: " << b << endl;
-                if(a > b){ cout << "the smaller value is: " << b << endl << "the larger value is: " << a << endl;}
-                else if(a < b){ cout << "the smaller value is: " << a << endl << "the larger value is: " << b << endl;}
-                else{ cout << "the numbers are equal" << endl; }
+bool existingUnit(string unit){
+        string units[] = {"m", "cm", "in", "ft"};
+        
+        for(string u:units){
+                if(unit == u){return true;}
+        }
+
+        return false;
+}
+
+
+double Convert(double &n, string &unit){
+        if(unit == "cm"){return n/100;}
+        else if(unit == "in"){return n*0.0254;}
+        else if(unit == "ft"){return n*0.3048;}
+        return n;
+}
+
+
+int main(){
+        vector<double> values{};
+        double n, sum = {0};
+        double smallest = std::numeric_limits<double>::infinity();
+        double largest = -1;
+        string unit;
+        
+        cout << "(value, unit): ";
+        while(cin >> n >> unit){
+                //Read(n, unit);
+                if(existingUnit(unit)){
+                double converted = {Convert(n, unit)};
+                sum += converted;
+                values.push_back(converted);
+
+                if(converted > largest ){
+                        cout << converted << "m the largest so far\n";
+                        largest = converted;
+                }
+                else if(converted  < smallest){
+                        cout << converted << "m the smallest so far\n";
+                        smallest = converted;
+                }
+                else{ cout << n << "\n"; }
+                }
+                else{
+                        cout << "Couldn't handle the given unit! Try it again with another one, please!\n";
+                }
+                cout << "(value, unit): ";
+        }
+        
+        cout << endl;
+        sort(values.begin(), values.end());
+        cout << "Largest value entered: " << largest << "m\n";
+        cout << "Smallest value entered: " << smallest << "m\n";
+        cout << "Number of values entered: " << values.size() << "\n";
+        cout << "List of values entered: \n";
+        for(double n:values){
+                cout << n << "m\n";
         }
         
         return 0;
